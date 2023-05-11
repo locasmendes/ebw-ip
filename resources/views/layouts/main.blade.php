5ab3570596caa42da41a1a680a7a624e3227c66f
locasmendes <lukinhazmm@gmail.com>
2023-05-11 02:00:14 -0300

    Atualiando a versão do Laravel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,8 +14,10 @@
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     <script src="{{url('js/jquery-3.6.4.min.js')}}" type="text/javascript"></script>
-    @vite('resources/plugins/owl/css/owl.carousel.css')
-    @vite('resources/plugins/owl/js/owl.carousel.min.js')
+    <link rel="stylesheet" href="{{url('css/owl/owl.carousel.css')}}">
+    <script src="{{url('js/owl/owl.carousel.min.js')}}" type="text/javascript"></script>
+    {{--@vite('resources/plugins/owl/css/owl.carousel.css')
+    @vite('resources/plugins/owl/js/owl.carousel.min.js')--}}
 </head>
 <body class="" style="	-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;">
 <div class="main" style="min-height: 100vh; background-color: @yield('main-bg-color','#000000');">
